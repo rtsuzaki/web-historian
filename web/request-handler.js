3b21e7a759ac588d58e5f49596039237b8e8f01e
rtsuzaki <rtsuzaki10@gmail.com>
2018-05-16 15:14:42 -0700

    initial commit. successful get and post request

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,59 @@
 var path = require('path');
+var fs = require('fs');
+var url = require('url');
 var archive = require('../helpers/archive-helpers');
+var qs = require('querystring');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'GET') {
+    console.log('in GET');
+    if (req.url === '/') {
+      fs.readFile(archive.paths.siteAssets + '/index.html', function(err, data) {
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          return res.end("404 Not Found");
+        }  
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
+    }
+    if (req.url === '/styles.css') {
+      fs.readFile(archive.paths.siteAssets + '/styles.css', function(err, data) {
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          return res.end("404 Not Found");
+        }  
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(data);
+        return res.end();
+      });
+    }
+  } else if (req.method === 'POST') {
+    console.log('in POST');
+    var body = '';
+    var post = '';
+    req.on('data', function (data) {
+      body += data;
+
+      // Too much POST data, kill the connection!
+      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+      if (body.length > 1e6) {
+        req.connection.destroy();
+      }
+    });
+    req.on('end', function () {
+      post = qs.parse(body);
+      // TODO
+
+
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('you requested to archive URL = ' + post.url);
+      return res.end();
+    });
+  } else {
+    console.log('---------> requestHandler');
+    res.end(archive.paths.list);
+  }
 };
